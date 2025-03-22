2364e78d79584a8e6df2230bc98358fc8022c40a
tiffany clark <tiffanymarie9811@gmail.com>
2025-03-22 17:57:04 -0400

    making the interface pages

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,12 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+  login: string;
+  id: number;
+  avatar_url: string;
+  html_url: string;
+  followers: number;
+  following: number;
+  public_repos: number;
+  site_admin: boolean;
+  type: string;
+}
